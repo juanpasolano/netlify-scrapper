b7d86b714778f96a0f1e052c64320ed0b1368f2b
Juan Pablo Solano <juanpa_solano@hotmail.com>
2019-03-25 13:00:00 -0500

    data got

--- src/srcap-util.js
+++ src/srcap-util.js
@@ -3,5 +3,19 @@ const $ = require("cheerio");
 const url = "https://www.asxenergy.com.au/";
 
 rp(url).then(html => {
-  console.log($('#home-prices', html));
+  const table = $("#home-prices", html);
+  const headers = [];
+  const content = [[],[],[],[]];
+  table.find("thead td").each((i, el) => {
+    headers.push($(el).text() || "year");
+  });
+  table.find("tbody tr").each((i, el) => {
+    el.children.forEach(td => {
+      const text = $(td).text().trim()
+      console.log(text);
+      if(text) content[i].push(text);
+    })
+  });
+  console.log(headers);
+  console.log(content);
 });
