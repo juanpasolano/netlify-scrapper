249ff3b14366f6ce65f668b185cef29dd6b1a320
Juan Pablo Solano <juanpa_solano@hotmail.com>
2019-03-25 13:06:24 -0500

    updates

--- functions/scrap.js
+++ functions/scrap.js
@@ -1,4 +1,26 @@
-const {fetchData} = require("../../src/scrap-util")
+const rp = require("request-promise");
+const $ = require("cheerio");
+const url = "https://www.asxenergy.com.au/";
+
+export const fetchData = () => {
+  return rp(url).then(html => {
+    const table = $("#home-prices", html);
+    const headers = [];
+    const content = [[],[],[],[]];
+    table.find("thead td").each((i, el) => {
+      headers.push($(el).text() || "year");
+    });
+    table.find("tbody tr").each((i, el) => {
+      el.children.forEach(td => {
+        const text = $(td).text().trim()
+        console.log(text);
+        if(text) content[i].push(text);
+      })
+      return {headers, content}
+    });
+  });
+} 
+
 
 exports.handler = function(event, context, callback) {
   fetchData().then(response => {
